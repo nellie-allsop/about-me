c5b1204a86782451d92131b4581a18eb27fb3fb6
Nellie Allsop <nellie.allsop@gmail.com>
2023-09-19 11:38:02 +0100

    Make 11:38 update

---
+++ app.js
@@ -0,0 +1,59 @@
+let myName = prompt("Hi there friend, what's your name?");
+message = "Hi there " + myName + "👋";
+
+alert(message);
+
+let firstQuestion = prompt(
+	"Ok, I'm going to be quizzing you about me today! Please answer yes or no to each of the following statements. Firstly: My middle name is Grace"
+);
+
+let secondQuestion = prompt("I used to have a guinea pig called Derek");
+
+let thirdQuestion = prompt("I have travelled to America");
+
+let fourthQuestion = prompt("I hate minestrone soup");
+
+let fifthQuestion = prompt("I have two siblings");
+
+// console.log("Hi there " + myName);
+
+// prompt(message) = ;
+
+// let answer1 = confirm("Are you ok?");
+
+// if (answer1 === true) {
+// 	console.log("You are ok");
+// } else {
+// 	console.log("You are not okay.");
+// }
+
+// console.log(answer1);
+
+// let first = true;
+// let second = false;
+// let third = true;
+
+// if (first && third) {
+// 	console.log("first and third were both true");
+// } else if (first || second) {
+// 	console.log("The first or second was true");
+// } else if (third) {
+// 	console.log("first and second were flas, but the thirs is true");
+// } else {
+// 	console.log("Is anything real?");
+// }
+
+// let color = prompt("What is favourite colour?");
+
+// console.log(color);
+// console.log(color.toLowerCase());
+
+// switch (color.toLowerCase()) {
+// 	case "red":
+// 		console.log("Your fav colour is red");
+// 		break;
+// 	case "blue":
+// 		console.log("Your fav colour is blue");
+// 		break;
+// 		deafault: console.log("I don't what kind of strange colour you picked!");
+// }
