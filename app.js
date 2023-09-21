63ee6b966bb4f764630b837a1839cfe13ea17e9c
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-09-21 15:24:09 +0100

    Add score and experiment with question 7

--- app.js
+++ app.js
@@ -1,148 +1,157 @@
+`use strict`;
 let myName = prompt("Hi there friend, what's your name?");
 message = "Hi there " + myName + "👋";
 
 alert(message);
 
 function startQuiz() {
-	let userPoints = 0;
+  let userPoints = 0;
 
-	let firstQuestion = prompt(
-		"Ok, I'm going to be quizzing you about me today! Please answer yes or no to each of the following statements. Firstly: My middle name is Grace"
-	);
+  let firstQuestion = prompt(
+    "Ok, I'm going to be quizzing you about me today! Please answer yes or no to each of the following statements. Firstly: My middle name is Grace"
+  );
 
-	if (
-		firstQuestion.toLowerCase() === "yes" ||
-		firstQuestion.toLowerCase() === "y"
-	) {
-		message = "Incorrect! ❌";
-	} else if (
-		firstQuestion.toLowerCase() === "no" ||
-		firstQuestion.toLowerCase() === "n"
-	) {
-		message = "Correct! ✅";
-	} else {
-		message = "That is not a valid response";
-	}
+  if (
+    firstQuestion.toLowerCase() === "yes" ||
+    firstQuestion.toLowerCase() === "y"
+  ) {
+    message = "Incorrect! ❌";
+  } else if (
+    firstQuestion.toLowerCase() === "no" ||
+    firstQuestion.toLowerCase() === "n"
+  ) {
+    message = "Correct! ✅";
+    userPoints++;
+  } else {
+    message = "That is not a valid response";
+  }
 
-	// console.log(message);
-	alert(message);
+  // console.log(message);
+  alert(message);
 
-	let secondQuestion = prompt("I used to have a guinea pig called Derek");
+  let secondQuestion = prompt("I used to have a guinea pig called Derek");
 
-	if (
-		secondQuestion.toLowerCase() === "yes" ||
-		secondQuestion.toLowerCase() === "y"
-	) {
-		message = "Correct! ✅";
-	} else if (
-		secondQuestion.toLowerCase() === "no" ||
-		secondQuestion.toLowerCase() === "n"
-	) {
-		message = "Incorrect! ❌";
-	} else {
-		message = "That is not a valid response";
-	}
+  if (
+    secondQuestion.toLowerCase() === "yes" ||
+    secondQuestion.toLowerCase() === "y"
+  ) {
+    message = "Correct! ✅";
+    userPoints++;
+  } else if (
+    secondQuestion.toLowerCase() === "no" ||
+    secondQuestion.toLowerCase() === "n"
+  ) {
+    message = "Incorrect! ❌";
+  } else {
+    message = "That is not a valid response";
+  }
 
-	// console.log(message);
-	alert(message);
+  // console.log(message);
+  alert(message);
 
-	let thirdQuestion = prompt("I have travelled to America");
+  let thirdQuestion = prompt("I have travelled to America");
 
-	if (
-		thirdQuestion.toLowerCase() === "yes" ||
-		thirdQuestion.toLowerCase() === "y"
-	) {
-		message = "Incorrect! ❌";
-	} else if (
-		thirdQuestion.toLowerCase() === "no" ||
-		thirdQuestion.toLowerCase() === "n"
-	) {
-		message = "Correct! ✅";
-	} else {
-		message = "That is not a valid response";
-	}
+  if (
+    thirdQuestion.toLowerCase() === "yes" ||
+    thirdQuestion.toLowerCase() === "y"
+  ) {
+    message = "Incorrect! ❌";
+  } else if (
+    thirdQuestion.toLowerCase() === "no" ||
+    thirdQuestion.toLowerCase() === "n"
+  ) {
+    message = "Correct! ✅";
+    userPoints++;
+  } else {
+    message = "That is not a valid response";
+  }
 
-	// console.log(message);
-	alert(message);
+  // console.log(message);
+  alert(message);
 
-	let fourthQuestion = prompt("I hate minestrone soup");
+  let fourthQuestion = prompt("I hate minestrone soup");
 
-	if (
-		fourthQuestion.toLowerCase() === "yes" ||
-		fourthQuestion.toLowerCase() === "y"
-	) {
-		message = "Correct! ✅";
-	} else if (
-		fourthQuestion.toLowerCase() === "no" ||
-		fourthQuestion.toLowerCase() === "n"
-	) {
-		message = "Incorrect! ❌";
-	} else {
-		message = "That is not a valid response";
-	}
+  if (
+    fourthQuestion.toLowerCase() === "yes" ||
+    fourthQuestion.toLowerCase() === "y"
+  ) {
+    message = "Correct! ✅";
+    userPoints++;
+  } else if (
+    fourthQuestion.toLowerCase() === "no" ||
+    fourthQuestion.toLowerCase() === "n"
+  ) {
+    message = "Incorrect! ❌";
+  } else {
+    message = "That is not a valid response";
+  }
 
-	// console.log(message);
-	alert(message);
+  // console.log(message);
+  alert(message);
 
-	let fifthQuestion = prompt("I have two siblings");
+  let fifthQuestion = prompt("I have two siblings");
 
-	if (
-		fifthQuestion.toLowerCase() === "yes" ||
-		fifthQuestion.toLowerCase() === "y"
-	) {
-		message = "Incorrect! ❌";
-	} else if (
-		fifthQuestion.toLowerCase() === "no" ||
-		fifthQuestion.toLowerCase() === "n"
-	) {
-		message = "Correct! ✅";
-	} else {
-		message = "That is not a valid response";
-	}
+  if (
+    fifthQuestion.toLowerCase() === "yes" ||
+    fifthQuestion.toLowerCase() === "y"
+  ) {
+    message = "Incorrect! ❌";
+  } else if (
+    fifthQuestion.toLowerCase() === "no" ||
+    fifthQuestion.toLowerCase() === "n"
+  ) {
+    message = "Correct! ✅";
+    userPoints++;
+  } else {
+    message = "That is not a valid response";
+  }
 
-	// console.log(message);
-	alert(message);
+  // console.log(message);
+  alert(message);
 
-	let answer = 6;
-	let sixthQuestion = prompt(
-		"Next up, can you guess how many pet guinea pigs I've had across my lifetime? Choose a number between 1 and 10"
-	);
+  let answer = 6;
+  let sixthQuestion = prompt(
+    "Next up, can you guess how many pet guinea pigs I've had across my lifetime? Choose a number between 1 and 10"
+  );
 
-	if (sixthQuestion <= 5) {
-		message = "That's too low!";
-	} else if (sixthQuestion >= 7) {
-		message = "That's too high!";
-	} else {
-		message = "That's correct!";
-	}
-	// console.log(message);
-	alert(message);
+  if (sixthQuestion <= 5) {
+    let mess = "That's too low!";
+  } else if (sixthQuestion >= 7) {
+    mess = "That's too high!";
+  } else {
+    mess = "That's correct!";
+    userPoints++;
+  }
+  // console.log(message);
+  alert(mess);
 
-	for (let i = 1; i <= 4; i++) {
-		console.log(i);
-	}
+  for (let i = 1; i <= 4; i++) {
+    console.log(i);
+  }
 
-	let seventhQuestion = prompt(
-		"And finally, I have three people in my family (other than me!) Can you guess any of their names?"
-	).toLowerCase();
+  let seventhQuestion = prompt(
+    "And finally, I have three people in my family (other than me!) Can you guess any of their names?"
+  ).toLowerCase();
 
-	let family = ["Claire", "Simon", "Alfie"];
+  let family = ["Claire", "Simon", "Alfie"];
 
-	for (let i = 0; i < family.length; i++) {
-		if (seventhQuestion === family[i]) {
-			alert("Yes well done");
-			userPoints++;
-		}
-	}
+  for (let a = 0; a < family.length; a++) {
+    if (seventhQuestion === family[a]) {
+      alert("Yes well done");
+      userPoints++;
+    } else if (seventhQuestion !== family[a]) {
+      alert("That's not correct.");
+    }
+  }
 
-	alert(
-		message +
-			" And, that's the end of the quiz " +
-			myName +
-			" If you were interested, my family members are called Claire, Simon and Alfie. At the end of that quiz, you have " +
-			userPoints +
-			" out of 7 points"
-	);
+  alert(
+    message +
+      " And, that's the end of the quiz " +
+      myName +
+      " If you were interested, my family members are called Claire, Simon and Alfie. At the end of that quiz, you have " +
+      userPoints +
+      " out of 7 points"
+  );
 }
 
 // For loop to limit four responses
